a6c997e4cc1ff5fff2801da6cda6b07e85353eac
Yahya ElAraby <yahya.elaraby@wxora.com>
2024-06-07 21:37:08 +0300

    chore: add rate limiter from @nest.js/throttler

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,33 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { OpenAIModule } from './openai/openai.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.MONGO_URI),
+    ThrottlerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => [
+        {
+          ttl: parseInt(config.get('THROTTLE_TTL') || '60000'),
+          limit: parseInt(config.get('THROTTLE_LIMIT') || '10'),
+        },
+      ],
+    }),
     AuthModule,
+    OpenAIModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
   ],
 })
 export class AppModule {}
